b7ceb10ff29e85ed0f91bf72fdaa475455166732
Suzi-Lawson <law18008@byui.edu>
2019-06-08 23:30:40 -0600

    new stuff

---
+++ assignments/lesson-7/scripts/currentdate.js
@@ -0,0 +1,21 @@
+(function() {
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    Date.prototype.getMonthName = function() {
+        return months[ this.getMonth() ];
+    };
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
+})();
+
+var today = new Date();
+var ww = today.getDayName()
+var dd = today.getDate();
+var mm = today.getMonthName(); //January is 0!
+var yyyy = today.getFullYear();
+
+today = ww+ ', ' +dd+' '+mm+' '+yyyy;
+document.write(today);
