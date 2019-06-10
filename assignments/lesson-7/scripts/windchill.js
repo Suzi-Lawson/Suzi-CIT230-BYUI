ee85d09dbf95289f8a82b4a4d50b86fa21e5f5e4
Suzi-Lawson <law18008@byui.edu>
2019-06-10 02:49:34 -0600

    still won't work

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,8 +1,7 @@
 function windChill() {
-	var tempF = parseFloat(document.getElementById('temp').value);
-	var speed = parseFloat(document.getElementById('windspeed').value);	
-    var f = 0
-
-    f = (35.74 + 0.6215 * t - 35.75 * (Math.pow(s,0.16)) + 0.4275 * t * (Math.pow(s,0.16))).toFixed(2);
-    document.getElementById('windchill').innerHTML = f;
+	var t = parseFloat(document.getElementById('temp').value);
+	var s = parseFloat(document.getElementById('windspeed').value);	
+    var f = (35.74 + 0.6215 * t - 35.75 * (Math.pow(s,0.16)) + 0.4275 * t * (Math.pow(s,0.16))).toFixed(2);
+    return f;
 }
+document.getElementById('windchill').innerHTML = windChill();
