b723761a382f4a33b36b903625cd428161f8e16d
Suzi-Lawson <law18008@byui.edu>
2019-06-10 02:30:23 -0600

    try again

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -4,5 +4,5 @@ function windChill() {
     var f = 0
 
     f = (35.74 + 0.6215 * t - 35.75 * (Math.pow(s,0.16)) + 0.4275 * t * (Math.pow(s,0.16))).toFixed(2);
+    document.getElementById('windchill').innerHTML = f;
 }
-document.getElementById('windchill').innerHTML = f;
