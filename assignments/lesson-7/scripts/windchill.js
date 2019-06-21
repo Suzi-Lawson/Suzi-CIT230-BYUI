96bdb3a1c173d34f23b660e11abc0dd4648c46b5
Suzi-Lawson <law18008@byui.edu>
2019-06-20 20:57:08 -0600

    basic changes

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,6 +1,6 @@
 function windChill() {
-	var t = parseFloat(document.getElementById('temp').value);
-	var s = parseFloat(document.getElementById('windspeed').value);	
+	var t = parseFloat(document.getElementById('temp').innerhtml);
+	var s = parseFloat(document.getElementById('windspeed').innerhtml);	
     var f = (35.74 + 0.6215 * t - 35.75 * (Math.pow(s,0.16)) + 0.4275 * t * (Math.pow(s,0.16))).toFixed(2);
     return f;
 }
