43e1f3ae4f7f00163c0df48e7cd44b97b0fd7317
Suzi-Lawson <law18008@byui.edu>
2019-06-29 03:03:38 -0600

    changed icon info back

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -14,7 +14,7 @@ weatherObject.onload = function() {
 const iconcode = weatherInfo.weather[0].icon;
 const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
 console.log(icon_path);
-document.getElementById('weather_iconOne').innerHTML = weatherInfo.weather[0].icon;
+document.getElementById('weather_iconOne').src = icon_path;
 document.getElementById('weather_iconTwo').src = icon_path;
 document.getElementById('weather_iconThree').src = icon_path;
 document.getElementById('weather_iconFour').src = icon_path;
