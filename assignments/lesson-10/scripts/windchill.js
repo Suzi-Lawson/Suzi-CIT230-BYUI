6a9f3f7c8aed57486aaedd40b19a0010bd3c28f1
Suzi-Lawson <law18008@byui.edu>
2019-06-29 03:58:05 -0600

    frustrating like crazy

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,4 +1,5 @@
 var temp = parseFloat(document.getElementById('currentTemp').innerhtml);
 var speed = parseFloat(document.getElementById('windSpeed').innerhtml);	
-var wind = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
+var wind = 0
+wind = (35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16)).toFixed(2);
 document.getElementById('windChill').innerHTML = wind;
