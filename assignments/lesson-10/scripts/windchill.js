3160738662b007dcf680efc80db569019fab300d
Suzi-Lawson <law18008@byui.edu>
2019-06-29 03:44:53 -0600

    so frustrated

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,7 +1,4 @@
-function windChill() {
-	var t = parseFloat(document.getElementById('currentTemp').innerhtml);
-	var s = parseFloat(document.getElementById('windSpeed').innerhtml);	
-    var f = (35.74 + 0.6215 * t - 35.75 * (Math.pow(s,0.16)) + 0.4275 * t * (Math.pow(s,0.16))).toFixed(2);
-    return f;
-}
-document.getElementById('windChill').innerHTML = windChill();
+var temp = parseFloat(document.getElementById('currentTemp').innerhtml);
+var speed = parseFloat(document.getElementById('windSpeed').innerhtml);	
+var wind = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
+document.getElementById('windChill').innerHTML = wind;
