ecd559d37e258975cd0742e54539ea2fea57815a
Suzi-Lawson <law18008@byui.edu>
2019-06-29 00:14:37 -0600

    fixed id

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET", "//api.openweathermap.org/data/2.5/group?id=5604473,703448,2643743&units=metric", true);
+weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?id=5604473,&units=imperial", true);
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
