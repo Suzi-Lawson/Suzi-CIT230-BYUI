2e9c1685c14be4ca1428a321cfee4900f63abd6b
Suzi-Lawson <law18008@byui.edu>
2019-06-28 22:45:23 -0600

    fixed some stuff

--- assignments/lesson-10/json-start/JSON-start/js/scripts.js
+++ assignments/lesson-10/json-start/JSON-start/js/scripts.js
@@ -1,7 +1,8 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("Get", "//api.openweathermap.org/data/2.5/weather?zip=83127,us&appid=b4f53c29c600d4a6c578dd81d9f2a254");
+weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=83127,us&appid=b4f53c29c600d4a6c578dd81d9f2a254", true);
 weatherObject.send();
 weatherObject.onload = function() {
-    let weatherinfo = JSON.parse(weatherObject.responseText);
+    let weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
+    
 } // end of onload
