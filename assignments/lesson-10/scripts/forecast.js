12fddf2aebc6690b271a7960300a830eda0b5fc2
Suzi-Lawson <law18008@byui.edu>
2019-06-29 19:25:20 -0600

    maybe this will work

--- assignments/lesson-10/scripts/forecast.js
+++ assignments/lesson-10/scripts/forecast.js
@@ -2,9 +2,9 @@ const weatherForcast = new XMLHttpRequest();
 weatherForcast.open("GET", "//api.openweathermap.org/data/2.5/forecast?zip=83263,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
 weatherForcast.send();
 weatherForcast.onload = function() {
-   
+    let weatherInfo = JSON.parse(weatherForcast.responseText);
     
-    const iconcode = weatherInfo.weather[0].icon;
+const iconcode = weatherInfo.weather[0].icon;
 const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
 console.log(icon_path);
 document.getElementById('weather_iconOne').src = icon_path;
