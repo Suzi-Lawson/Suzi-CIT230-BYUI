c58dbfb021c6c4d14fc32c63a54bb2ec04ea4e3c
Suzi-Lawson <law18008@byui.edu>
2019-06-30 00:15:27 -0600

    more changes

--- assignments/lesson-10/scripts/forecast.js
+++ assignments/lesson-10/scripts/forecast.js
@@ -2,7 +2,7 @@ const weatherForcast = new XMLHttpRequest();
 weatherForcast.open("GET", "//api.openweathermap.org/data/2.5/forecast?zip=83263,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
 weatherForcast.send();
 weatherForcast.onload = function() {
-    let weatherInfo = JSON.parse(weatherForcast.responseText);
+    let forcastInfo = JSON.parse(weatherForcast.responseText);
     
 
 }
