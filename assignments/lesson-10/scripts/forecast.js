9a4cffe2f9b2fef45b6fc4ab9f17659ec7bef600
Suzi-Lawson <law18008@byui.edu>
2019-06-30 00:14:25 -0600

    commit

--- assignments/lesson-10/scripts/forecast.js
+++ assignments/lesson-10/scripts/forecast.js
@@ -4,12 +4,5 @@ weatherForcast.send();
 weatherForcast.onload = function() {
     let weatherInfo = JSON.parse(weatherForcast.responseText);
     
-const iconcode = weatherInfo.weather[0].icon;
-const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-console.log(icon_path);
-document.getElementById('weather_iconOne').src = icon_path;
-document.getElementById('weather_iconTwo').src = icon_path;
-document.getElementById('weather_iconThree').src = icon_path;
-document.getElementById('weather_iconFour').src = icon_path;
-document.getElementById('weather_iconFive').src = icon_path;
+
 }
