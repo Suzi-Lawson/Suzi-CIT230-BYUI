66a4386e01a768bfe80b591c3bc60a84cc419566
Suzi-Lawson <law18008@byui.edu>
2019-07-01 23:36:33 -0600

    let's see if I got it right this time.

--- assignments/lesson-10/scripts/forecast.js
+++ assignments/lesson-10/scripts/forecast.js
@@ -5,69 +5,53 @@ weatherForecast.onload = function() {
     let forecastInfo = JSON.parse(weatherForecast.responseText);
     console.log(forcastInfo);
 
+    let fmonth = [];
+    let fday = [];
+    let ficon = [];
+    let fdesc = [];
+    let ftemp = [];
+    let day = 0
+
+    forecastInfo.list.forEach(x => {
+        if (x.dt_txt.includes('18:00:00')) {
+            fmonth[day] = x.dt_txt.substring(5, 7);
+            fday[day] = x.dt_txt.substring(8, 10);
+            ficon[day] = x.weather[0].icon;
+            fdesc[day] = x.weather[0].description;
+            ftemp[day] = Math.round(x.main.temp)
+
+            console.log(day, fmonth[day], fday[day], ficon[day], fdesc[day], ftemp[day])
+            day++;
+        }    
+    });
 
 
-    for (var i = 0; i < forecastInfo.list.length; i++) {
-        if (forecastInfo.list[i].dt_txt.match('18:00:00')) {
-            document.getElementById('dayOnetemp').innerHTML = forecastInfo.list[i].main.temp;
-            document.getElementById('dayOne').innerHTML = forecastInfo.list[i].dt;
-     
-        
-            let icon = "//openweathermap.org/img/w/" + forecastInfo.list[i].weather[0].icon + ".png";
-            let = desc = forecastInfo.list[i].weather[0].description;
-            document.getElementById('weather_iconOne').setAttribute('src', icon);
-            document.getElementById('weather_iconOne').setAttribute('atl', desc);  
-        }
-    }
-
-    for (var i = 8; i < 16; i++) {
-        if (forecastInfo.list[i].dt_txt == '18:00:00') {
-            document.getElementById('dayTwotemp').innerHTML = forecastInfo.list[i].main.temp;
-            document.getElementById('dayTwo').innerHTML = forecastInfo.list[i].dt;
-     
-            const iconcode = forecastInfo.list[i].weather[0].icon;
-            const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-            console.log(icon_path);
-            document.getElementById('weather_iconTwo').innerHTML = icon_path; 
-        }
-    }
-
-    for (var i = 16; i < 24; i++) {
-        if (forecastInfo.list[i].dt_txt == '18:00:00') {
-            document.getElementById('dayThreetemp').innerHTML = forecastInfo.list[i].main.temp;
-            document.getElementById('dayThree').innerHTML = forecastInfo.list[i].dt;
-     
-            const iconcode = forecastInfo.list[i].weather[0].icon;
-            const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-            console.log(icon_path);
-            document.getElementById('weather_iconThree').innerHTML = icon_path; 
-        }
-    }
-
-    for (var i = 24; i < 32; i++) {
-        if (forecastInfo.list[i].dt_txt == '18:00:00') {
-            document.getElementById('dayFourtemp').innerHTML = forecastInfo.list[i].main.temp;
-            document.getElementById('dayFour').innerHTML = forecastInfo.list[i].dt;
-     
-            const iconcode = forecastInfo.list[i].weather[0].icon;
-            const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-            console.log(icon_path);
-            document.getElementById('weather_iconFour').innerHTML = icon_path; 
-        }
-    }
-
-    for (var i = 32; i < 40; i++) {
-        if (forecastInfo.list[i].dt_txt == '18:00:00') {
-            document.getElementById('dayFivetemp').innerHTML = forecastInfo.list[i].main.temp;
-            document.getElementById('dayFive').innerHTML = forecastInfo.list[i].dt;
-     
-            const iconcode = forecastInfo.list[i].weather[0].icon;
-            const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-            console.log(icon_path);
-            document.getElementById('weather_iconFive').innerHTML = icon_path; 
-        }
-    }
-
+            var Months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+// Day One
+            document.getElementById('dayOne').innerHTML = Months[fmonth[0] - 1] + " " + fday[0];
+            document.getElementById('weather_iconOne').src = "//openweathermap.org/img/w/" + ficon[0] + ".png";
+            document.getElementById('weather_iconOne').atl = fdesc[0];  
+            document.getElementById('dayOnetemp').innerHTML = ftemp[0];
+// Day Two
+            document.getElementById('dayTwo').innerHTML = Months[fmonth[0] - 1] + " " + fday[1];
+            document.getElementById('weather_iconTwo').src = "//openweathermap.org/img/w/" + ficon[1] + ".png";
+            document.getElementById('weather_iconTwo').atl = fdesc[1];  
+            document.getElementById('dayTwotemp').innerHTML = ftemp[1];
+// Day Three
+            document.getElementById('dayThree').innerHTML = Months[fmonth[0] - 1] + " " + fday[2];
+            document.getElementById('weather_iconThree').src = "//openweathermap.org/img/w/" + ficon[2] + ".png";
+            document.getElementById('weather_iconThree').atl = fdesc[2];  
+            document.getElementById('dayThreetemp').innerHTML = ftemp[2];
+// Day Four
+            document.getElementById('dayFour').innerHTML = Months[fmonth[0] - 1] + " " + fday[3];
+            document.getElementById('weather_iconFour').src = "//openweathermap.org/img/w/" + ficon[3] + ".png";
+            document.getElementById('weather_iconFour').atl = fdesc[3];  
+            document.getElementById('dayFourtemp').innerHTML = ftemp[3];
+// Day Five
+            document.getElementById('dayFive').innerHTML = Months[fmonth[0] - 1] + " " + fday[4];
+            document.getElementById('weather_iconFive').src = "//openweathermap.org/img/w/" + ficon[4] + ".png";
+            document.getElementById('weather_iconFive').atl = fdesc[4];  
+            document.getElementById('dayFivetemp').innerHTML = ftemp[4];
 
 }
 
