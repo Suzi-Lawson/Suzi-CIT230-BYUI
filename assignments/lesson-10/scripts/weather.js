237a55f3eebd2235bb6857730a8dfc77aadea447
Suzi-Lawson <law18008@byui.edu>
2019-07-01 22:39:51 -0600

    let's see if it works

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
+weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?id=5604473,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
