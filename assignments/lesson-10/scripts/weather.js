f8e930ca73dc6f0b0af823d680bed883defd9f7b
Suzi-Lawson <law18008@byui.edu>
2019-07-02 20:16:24 -0600

    let's see if that fixes it.

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -10,3 +10,10 @@ weatherObject.onload = function() {
     document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 }
+
+/* wind chill equation */
+var temp = weatherInfo.main.temp;
+var speed = weatherInfo.wind.speed;
+var wind = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * temp) * Math.pow(speed,0.16);
+wind = Math.round(wind)
+document.getElementById('windChill').innerHTML = wind;
