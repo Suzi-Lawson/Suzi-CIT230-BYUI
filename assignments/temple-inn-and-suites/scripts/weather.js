1d12a4816545c293cec6fe0ef9a8c3b7468b5d3d
Suzi-Lawson <law18008@byui.edu>
2019-07-27 06:18:06 -0600

    does this work

--- assignments/temple-inn-and-suites/scripts/weather.js
+++ assignments/temple-inn-and-suites/scripts/weather.js
@@ -1,5 +1,94 @@
 const weatherObject = new XMLHttpRequest();
 
+weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=80528,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
+
+
+weatherObject.send();
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('fweatherDescr').innerHTML = weatherInfo.weather[0].description;
+    document.getElementById('fcurrentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('fhumidity').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('fwindSpeed').innerHTML = weatherInfo.wind.speed;
+
+
+/* wind chill equation */
+
+let temp = weatherInfo.main.temp;
+let speed = weatherInfo.wind.speed;
+document.getElementById('fwindchill').innerHTML = fwindChill();
+
+function fwindChill() {
+
+    var wind = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * temp) * Math.pow(speed,0.16);
+    return Math.round(wind)
+}
+}
+
+
+const weatherObject = new XMLHttpRequest();
+
+weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=92122,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
+
+
+weatherObject.send();
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('sweatherDescr').innerHTML = weatherInfo.weather[0].description;
+    document.getElementById('scurrentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('shumidity').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('swindSpeed').innerHTML = weatherInfo.wind.speed;
+
+
+/* wind chill equation */
+
+let temp = weatherInfo.main.temp;
+let speed = weatherInfo.wind.speed;
+document.getElementById('swindchill').innerHTML = swindChill();
+
+function swindChill() {
+
+    var wind = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * temp) * Math.pow(speed,0.16);
+    return Math.round(wind)
+}
+}
+/* hawaii */ 
+const weatherObject = new XMLHttpRequest();
+
+weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=96762,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
+
+
+weatherObject.send();
+weatherObject.onload = function() {
+    let weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('hweatherDescr').innerHTML = weatherInfo.weather[0].description;
+    document.getElementById('hcurrentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('hhumidity').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('hwindSpeed').innerHTML = weatherInfo.wind.speed;
+
+
+/* wind chill equation */
+
+let temp = weatherInfo.main.temp;
+let speed = weatherInfo.wind.speed;
+document.getElementById('hwindchill').innerHTML = hwindChill();
+
+function hwindChill() {
+
+    var wind = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * temp) * Math.pow(speed,0.16);
+    return Math.round(wind)
+}
+}
+
+/* redlands */
+const weatherObject = new XMLHttpRequest();
+
 weatherObject.open("GET", "//api.openweathermap.org/data/2.5/weather?zip=92374,us&appid=b4f53c29c600d4a6c578dd81d9f2a254&units=imperial", true);
 
 
@@ -8,19 +97,19 @@ weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
 
-    document.getElementById('weatherDescr').innerHTML = weatherInfo.weather[0].description;
-    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
-    document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
-    document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
+    document.getElementById('rweatherDescr').innerHTML = weatherInfo.weather[0].description;
+    document.getElementById('rcurrentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('rhumidity').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('rwindSpeed').innerHTML = weatherInfo.wind.speed;
 
 
 /* wind chill equation */
 
 let temp = weatherInfo.main.temp;
 let speed = weatherInfo.wind.speed;
-document.getElementById('windchill').innerHTML = windChill();
+document.getElementById('rwindchill').innerHTML = rwindChill();
 
-function windChill() {
+function rwindChill() {
 
     var wind = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * temp) * Math.pow(speed,0.16);
     return Math.round(wind)
